09a98f180e223485a7b3a637cf2eee4577fba760
Geoff <fox.geoffrey@gmail.com>
2018-02-13 17:42:23 -0800

    Added Contact Form with model binding

--- EmptyProj/Controllers/AppController.cs
+++ EmptyProj/Controllers/AppController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using EmptyProj.ViewModels;
 
 namespace EmptyProj.Controllers
 {
@@ -13,7 +14,15 @@ namespace EmptyProj.Controllers
             return View();
         }
 
+        [HttpGet("contact")]
         public IActionResult Contact()
+        {
+            ViewBag.Title = "Contact Us";
+            return View();
+        }
+
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
         {
             ViewBag.Title = "Contact Us";
             return View();
