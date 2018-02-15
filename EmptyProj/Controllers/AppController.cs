333c57536d97d7a7c85f044fdf19624f812410a7
Geoff <fox.geoffrey@gmail.com>
2018-02-14 16:02:51 -0800

    Added a NullMailService

--- EmptyProj/Controllers/AppController.cs
+++ EmptyProj/Controllers/AppController.cs
@@ -4,11 +4,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using EmptyProj.ViewModels;
+using EmptyProj.Services;
 
 namespace EmptyProj.Controllers
 {
     public class AppController : Controller
     {
+        private readonly IMailService _mailService;
+
+        public AppController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
         public IActionResult Index()
         {
             return View();
@@ -27,6 +34,9 @@ namespace EmptyProj.Controllers
             if (ModelState.IsValid)
             {
                 //Send Email
+                _mailService.SendMessage(model.Name, model.Subject, model.Message);
+                ViewBag.Message = "The message has been Sent. Thank you!";
+                ModelState.Clear();
             }
             else
             {
