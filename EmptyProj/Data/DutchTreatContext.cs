f9f41345596db05d2a1ed50cb1870ab2e787804d
Geoff <fox.geoffrey@gmail.com>
2018-02-16 18:17:11 -0800

    Added DutchTreatContext

---
+++ EmptyProj/Data/DutchTreatContext.cs
@@ -0,0 +1,15 @@
+﻿using DutchTreat.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmptyProj.Data
+{
+    public class DutchTreatContext : DbContext
+    {
+        DbSet<Product> Products { get; set; }
+        DbSet<Order> Orders { get; set; }
+    }
+}
